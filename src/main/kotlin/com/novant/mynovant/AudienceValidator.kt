e3c9f21cd324074db1b8fb70df6b1f87ac9b457e
Joseph Weissman <jweissman@pivotal.io>
2019-11-07 10:24:38 -0500

    Serving resources to token bearers

---
+++ src/main/kotlin/com/novant/mynovant/AudienceValidator.kt
@@ -0,0 +1,23 @@
+package com.novant.mynovant
+
+import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult
+import org.springframework.security.oauth2.core.OAuth2Error
+import org.springframework.security.oauth2.core.OAuth2TokenValidator
+import org.springframework.security.oauth2.jwt.Jwt
+
+
+class AudienceValidator : OAuth2TokenValidator<Jwt> {
+    private val error = OAuth2Error("invalid_token", "The required audience is missing", null)
+
+    override fun validate(jwt: Jwt): OAuth2TokenValidatorResult {
+        //The audience string must be updated to represent the resource server's resource ID
+        //When registering resources in SSO, resource ID is used as the first part of scopes, e.g. todo.read for the todo resource ID
+//        return if (jwt.getAudience().contains("todo")) {
+//            OAuth2TokenValidatorResult.success()
+//        } else {
+//            OAuth2TokenValidatorResult.failure(error)
+//        }
+        return OAuth2TokenValidatorResult.success()
+
+    }
+}
--- src/main/kotlin/com/novant/mynovant/DoctorController.kt
+++ src/main/kotlin/com/novant/mynovant/DoctorController.kt
@@ -3,20 +3,28 @@ package com.novant.mynovant
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@RestController("/api")
 class DoctorController {
+    @GetMapping("")
+    fun listApiOperations() = listOf("/doctors")
+
     @GetMapping("/doctors")
-    fun listDoctors() = listOf(
-        Doctor(1, "Strange", "Metaphysics"),
-        Doctor(2, "Who", "Chronodynamics"),
-        Doctor(3, "Challenger", "Geotraumatics"),
-        Doctor(4, "Jekyll", "Metamorphology"),
-        Doctor(5, "Mabuse", "Hypnosis"),
-        Doctor(6, "Lecter", "Psychiatry"),
-        Doctor(7, "Venkman", "Paranormal Investigation"),
-        Doctor(8, "Moreau", "Genotherapy"),
-        Doctor(9, "Jones", "Archaeology"),
-        Doctor(10, "Frankenstein", "Necromancy"),
-        Doctor(11, "Crusher", "General Practitioner")
-    )
+    fun listDoctors(): List<Doctor> {
+        println("LIST DOCS")
+        return listOf(
+                Doctor(1, "Strange", "Metaphysics"),
+                Doctor(2, "Who", "Chronodynamics"),
+                Doctor(3, "Challenger", "Geotraumatics"),
+                Doctor(4, "Jekyll", "Metamorphology"),
+                Doctor(5, "Mabuse", "Hypnosis"),
+                Doctor(6, "Lecter", "Psychiatry"),
+                Doctor(7, "Venkman", "Paranormal Investigation"),
+                Doctor(8, "Moreau", "Genotherapy"),
+                Doctor(9, "Jones", "Archaeology"),
+                Doctor(10, "Frankenstein", "Monstronomy"),
+                Doctor(11, "Crusher", "General Practitioner"),
+                Doctor(12, "Name", "Emergency Medical Hologram Mark I"),
+                Doctor(13, "Katz", "Therapist")
+        )
+    }
 }
